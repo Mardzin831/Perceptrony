d7d8dd98fea1c75062a471abb9ab9b805540a182
Mardzin <marcin010998@wp.pl>
2021-10-20 22:58:30 +0200

    Przed zmianą z arraya na listę

--- Perceptrony/MainWindow.xaml.cs
+++ Perceptrony/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Perceptrony
 {
     public partial class MainWindow : Window
     {
-        public int [,] array = new int [5, 7];
+        public int [] array = new int [35];
         public List<Array> examples = new List<Array>();
         public List<int> answers = new List<int>();
         public List<double> w = new List<double>();
@@ -53,22 +53,22 @@ namespace Perceptrony
             {
                 bt.Background = Brushes.Transparent;
             }
-            int a = Int32.Parse(bt.Tag.ToString()) / 10;
-            int b = Int32.Parse(bt.Tag.ToString()) % 10;
-            if(array[a, b] == 0)
+            int a = Int32.Parse(bt.Tag.ToString()) - 1;
+            if(array[a] == 0)
             {
-                array[a, b] = 1;
+                array[a] = 1;
             }
             else
             {
-                array[a, b] = 0;
+                array[a] = 0;
             }
-            predictNumber.Content = array[a, b];
+            predictNumber.Content = array[a];
         }
         private void AddExample(object sender, RoutedEventArgs e)
         {
             if(answerBox.Text.Length != 1)
             {
+                MessageBox.Show("Podaj odpowiedź do przykładu.");
                 return;
             }
             if (examples.Contains(array))
@@ -79,7 +79,7 @@ namespace Perceptrony
             examples.Add(array);
             answers.Add(Int32.Parse(answerBox.Text));
             string json = JsonConvert.SerializeObject(examples);
-            System.IO.File.WriteAllText("Przykłady.json", json);
+            File.WriteAllText("Przykłady.json", json);
 
         }
     }
